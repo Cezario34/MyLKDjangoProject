f0d443a8df0fe3ead12d0eb52c5635ca03507a48
Vladislav <ikeaq.qq@mail.ru>
2025-09-05 20:55:15 +0400

    W1-D2: urls + dashboard + healthz

--- lk_core/views.py
+++ lk_core/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'lk_core/base.html')
+    return render(request, 'lk_core/dashboard.html')
+
+def healthz(request):
+    return render(request, 'lk_core/healthz.html')
