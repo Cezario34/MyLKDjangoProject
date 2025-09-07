8e71ae345a28175d2ba9f5410681bcb8c7bba0a0
Vladislav <ikeaq.qq@mail.ru>
2025-09-07 21:07:35 +0400

    W1-D3: messages + PRG + dynamic award + healthz

--- lk_core/urls.py
+++ lk_core/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'lk_core'
+
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='dashboard'),
     path('healthz/', views.healthz, name='healthz'),
+    path('/award/<int:amount>/', views.award_amount, name='award'),
+    path('/award/plus10/', views.award_plus10, name='award_plus10'),
 ]
--- lk_core/views.py
+++ lk_core/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
+from django.contrib.messages.context_processors import messages
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_GET, require_POST
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseBadRequest, Http404, HttpResponseServerError
+
 
 # Create your views here.
+@require_GET
 def home(request):
-    return render(request, 'lk_core/dashboard.html')
+    balance = request.session.get("lk_balance", 0)
+    return render(request, 'lk_core/dashboard.html', {"balance": balance})
 
+@require_GET
 def healthz(request):
     return render(request, 'lk_core/healthz.html')
+
+@require_GET
+def award_amount(request,amount: int):
+    if not -1000 <= amount <= 1000:
+        return HttpResponseBadRequest('Баланс от 0 до 1000 должен быть')
+    request.session["lk_balance"] = request.session.get("lk_balance", 0) + amount
+    messages.success(request, f"Начислено {+amount}")
+    return redirect("lk_core:dashboard")
+
+@require_POST
+def award_plus10(request):
+    request.session["lk_balance"] = request.session.get("lk_balance", 0) + 10
+    messages.success(request, f"Начислено +10")
+    return redirect("lk_core:dashboard")
